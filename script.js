894c5926cbe5bdb755f27697aadc6d17641f2718
kauan-alves <kauam2010@hotmail.com>
2023-01-20 15:08:13 -0300

    feat(Botton function)

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
 const form = document.querySelector("#form-habits")
 const nlwSetup = new NLWSetup(form)
+const button = document.querySelector("header button")
 
-const data = {
-  run: ["01-01", "01-02", "01-06", "01-07", "01-08"],
-  takePills: ["01-03"],
-  journal: ["01-02"],
+button.addEventListener("click", add)
+
+function add() {
+  const today = "01/01"
+  const dayExists = nlwSetup.dayExists(today)
+
+  if (dayExists) {
+    alert("Dia já incluso 🔴")
+  }
+
+  nlwSetup.addDay(today)
 }
-
-nlwSetup.setData(data)
-nlwSetup.load()
